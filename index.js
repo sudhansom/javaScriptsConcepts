8c1f72a2494d18e667ccd679eadb6229ed400209
bhuwan <bkspoudel@gmail.com>
2022-03-10 17:47:12 +0100

    button clicked counted

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 array.forEach((element) => {
   setTimeout(() => {
     console.log(element);
-  }, element * 1000);
+  }, element * 100);
 });
 
 setTimeout(() => {
   console.log("element");
-}, 5000);
+}, 1000);
+
+function callMe() {
+  let a = 0;
+  document.getElementById("Button").addEventListener("click", function xyz() {
+    console.log("Button clicked...", ++a);
+  });
+}
+callMe();
