547e74e8bd65ac262a9b3df13c7f43c73838763e
bhuwan <bkspoudel@gmail.com>
2022-03-14 22:05:04 +0100

    changed to ternary

--- sumAll.js
+++ sumAll.js
@@ -1,10 +1,6 @@
 const sum = (a) => {
   return (b) => {
-    if (b) {
-      return sum(a + b);
-    } else {
-      return a;
-    }
+    return b ? sum(a + b) : a;
   };
 };
 console.log(sum(1)(2)(3)(4)());
