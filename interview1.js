e55d4e255c12902f0df6a3056a2725c5898d7bb2
bhuwan <bkspoudel@gmail.com>
2022-03-15 15:52:57 +0100

    changes accepted

--- interview1.js
+++ interview1.js
@@ -10,6 +10,7 @@ Array.prototype.contains = function (value) {
 
 const myArray = [1, 2, 3, 4, 5, 9, 7, 9, 9, 10];
 myArray.push(11);
+
 let timer = setTimeout(() => {
   console.log("here I am with you...");
 }, 5000);
