6467c5642058bf2ed3a6de4c518c8cda624aebd7
bhuwan <bkspoudel@gmail.com>
2022-03-16 23:31:03 +0100

    implemented my own filter method

---
+++ customFilter.js
@@ -0,0 +1,15 @@
+Array.prototype.customMap = function (callback) {
+  let myArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      myArray.push(this[i]);
+    }
+  }
+  return myArray;
+};
+const givenArray = [1, 2, 3, 4, 5];
+
+const result = givenArray.customMap((item) => {
+  return item % 2 == 0;
+});
+console.log(result);
