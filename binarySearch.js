a377ca7ce95fd1dde4f1b3f514fd6b348e7b7fe2
bhuwan <bkspoudel@gmail.com>
2022-05-06 06:42:32 +0200

    binary search implemented

---
+++ binarySearch.js
@@ -0,0 +1,17 @@
+console.log("Binary search:");
+const binarySearch = (arr, start = 0, end = 0, target) => {
+  if (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+    return binarySearch(arr, start, end, target);
+  }
+};
+
+const result = binarySearch([1, 2, 3, 4, 5, 6], 0, 5, 4);
+console.log(result);
--- promises.js
+++ promises.js
@@ -1,9 +1,14 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject("ooh no... smthing went wrong");
+    resolve(2);
   }, 1000);
 });
 
 console.log(promise);
 
-promise.then(console.log).catch(console.log);
+//promise.then(console.log).catch(console.log);
+const result = async () => {
+  const result = await promise;
+  console.log(result);
+};
+console.log(result);
